b6fff86dd6390f2adb36973ff1827bb40fc1be20
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-07 22:50:25 +0200

    init

---
+++ config/env.localsite.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    galleryViewPath: 'http://localhost/echarts-website/examples/${lang}/view.html?c=',
+    galleryEditorPath: 'http://localhost/echarts-website/examples/${lang}/editor.html?c=',
+    handbookPath: 'http://localhost/echarts-website/handbook/${lang}/',
+    websitePath: './',
+
+    imagePath: 'asset/img/',
+    gl: {
+        imagePath: 'asset/gl/img/'
+    },
+
+    releaseDestDir: path.resolve(__dirname, '../../echarts-website'),
+    ecWWWGeneratedDir: path.resolve(__dirname, '../../echarts-www/_generated')
+};
